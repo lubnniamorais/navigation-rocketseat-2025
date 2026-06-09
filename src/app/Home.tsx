0150db2615fbf6f4306e1a0cce36b031e6e85204
lubnniamorais <lubnnia@gmail.com>
2026-06-09 09:49:31 -0300

    feat: Create  do bottom navigation

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -13,7 +13,7 @@ export function Home({ navigation }: StackRoutesProps<'home'>) {
         <Title>Home</Title>
         <ButtonIcon
           name='add-circle'
-          onPress={() => navigation.navigate('product')}
+          onPress={() => navigation.navigate('product', { id: '1' })}
         />
       </Header>
     </View>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,13 @@
 import { NavigationContainer } from '@react-navigation/native';
 
 import { StackRoutes } from './StackRoutes';
+import { BottomRoutes } from './BottomRoutes';
 
 export function Routes() {
   return (
     // O NavigationContainer vai disponibilizar todas as rotas que estão disponiveis
     <NavigationContainer>
-      <StackRoutes />
+      <BottomRoutes />
     </NavigationContainer>
   );
 }
